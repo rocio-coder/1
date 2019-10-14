ce8c47128248a8441a2d0372d885e538ea351bf2
rocio-corder <suarez.rocio96@gmail.com>
2019-10-14 20:34:52 -0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,43 @@
+public class App {
+
+    public static void main(String[] args) {
+//        \\int [] numbers= new int[5];
+        //   numbers= {1,2,3,4,5};
+        //int i=0;
+        //while(i<numbers.length)
+        //  System.out.println("valor:"+numbers[i]);
+        // i++;
+        //int i;
+        //int[] v = {1, 2, 3, 4, 5, 6};
+        //int[] b = new int[6];
+        //for (i = 0; i < v.length; i++)
+        //  System.out.println(v[i]);
+
+        //for (i = 0; i < v.length; i++) {
+        //  b[i] = v[v.length - i - 1];
+        //}
+        //for (i = 0; i < v.length; i++)
+        //  System.out.println(b[i]);
+
+        //{
+          //  int[][] mat = {{5, 5, 5, 5}, {7, 7, 7, 7}, {0, 0, 0, 0}, {9, 0, 9, 9}};
+            //int i;
+            //int j;
+            //int cont = 0;
+            //for (i = 0; i < 4; i++) {
+              //  for (j = 0; j < 4; j++) {
+                //    if (mat[i][j] == 0)
+                  //      cont++;
+                //}
+                //System.out.println(cont);
+
+
+          //  }
+
+        //}
+
+
+
+
+    }
+}
